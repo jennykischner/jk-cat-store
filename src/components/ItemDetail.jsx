b64cd78c643184b7c20e062dc4289311af9b55ca
jennykischner <kischnerjennifer@gmail.com>
2023-08-29 18:54:46 -0300

    add loading

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,19 +1,21 @@
-import React, { useState, useContext } from 'react'; 
-import Count from './Count';
-import { Card } from 'react-bootstrap'; 
-import { CartContext } from '../context/CartContextProvide';
-
+import React, { useEffect, useState, useContext } from "react";
+import Count from "./Count";
+import { Card } from "react-bootstrap";
+import { CartContext } from "../context/CartContextProvide";
+import { MdCached } from "react-icons/md";
 
 const ItemDetail = ({ product }) => {
-  const [selectedQuantity, setSelectedQuantity] = useState(1); 
+  const [selectedQuantity, setSelectedQuantity] = useState(1);
   const [addedToCart, setAddedToCart] = useState(false);
+  const [showImage, setShowImage] = useState(false);
+  const [loading, setLoading] = useState(true); 
 
-  const { addItem } = useContext(CartContext); 
+  const { addItem } = useContext(CartContext);
 
   const handleCountChange = (count) => {
     setSelectedQuantity(count);
   };
- 
+
   const handleAddToCart = (product) => {
     addItem({
       id: product.id,
@@ -24,24 +26,53 @@ const ItemDetail = ({ product }) => {
     setAddedToCart(true);
   };
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false); 
+      setShowImage(true);
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   if (!product) {
-    return <p>Cargando...</p>; 
+    return (
+      <div className="center-container">
+        {loading ? <MdCached size="36" className="loading-icon" /> : null}
+      </div>
+    );
   }
+
 
   return (
     <div className="center-container">
-      <Card key={product.id} className="product-card"> 
-        <Card.Img className="product-image" variant="top" src={product.image} alt={product.name}  />
+     <Card
+        key={product.id}
+        className={`product-card ${showImage ? 'show' : ''}`}
+      >
+        <Card.Img
+          className={`product-image ${showImage ? "show" : ""}`}
+          variant="top"
+          src={product.image}
+          alt={product.name}
+        />
         <Card.Body>
           <Card.Title className="product-title">{product.name}</Card.Title>
-          <Card.Text className="product-description">{product.description}</Card.Text>
-          <p>Stock disponible: {product.stock}</p> 
+          <Card.Text className="product-description">
+            {product.description}
+          </Card.Text>
+          <p>Stock disponible: {product.stock}</p>
           <Count
             initialCount={selectedQuantity}
             onCountChange={handleCountChange}
             maxCount={product.stock}
           />
-          <button onClick={() => handleAddToCart(product)} className="add-to-cart-button">Añadir al carrito</button>
+          <button
+            onClick={() => handleAddToCart(product)}
+            className="add-to-cart-button"
+          >
+            Añadir al carrito
+          </button>
           {addedToCart && <p>Producto añadido al carrito</p>}
         </Card.Body>
       </Card>
